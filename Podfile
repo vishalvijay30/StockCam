17a291f79cb7ef71db6303f58a35d6142bda3bd0
vishalvijay30 <sinjaybrothers@gmail.com>
2017-08-24 14:17:41 -0400

    creat podfile, get working request/response from google cloud vision

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'StockCam' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for StockCam
+  pod 'SwiftyJSON', '~> 3.1'
+end
--- StockCam/View2.swift
+++ StockCam/View2.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 import AVFoundation
+import SwiftyJSON
 
 class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    let session = URLSession.shared
     
+    var image : UIImage?
     var captureSession : AVCaptureSession?
     var stillImageOutput : AVCaptureStillImageOutput?
     var previewLayer : AVCaptureVideoPreviewLayer?
 
     @IBOutlet weak var cameraView: UIView!
+    
+    var googleAPIKey = "AIzaSyDPxcJyGQOvNMP4wxvSF7OJUI7hhPjIv7w"
+    var googleURL : URL {
+        return URL(string: "https://vision.googleapis.com/v1/images:annotate?key=\(googleAPIKey)")!
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,6 +79,7 @@ class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationCont
     @IBOutlet weak var tempImageView: UIImageView!
     
     func didPressTakePhoto() {
+        //var image : UIImage?
         if let videoConnection = stillImageOutput?.connection(withMediaType: AVMediaTypeVideo) {
             videoConnection.videoOrientation = AVCaptureVideoOrientation.portrait
             stillImageOutput?.captureStillImageAsynchronously(from: videoConnection, completionHandler: {
@@ -81,31 +90,142 @@ class View2: UIViewController, UIImagePickerControllerDelegate, UINavigationCont
                     let dataProvider = CGDataProvider(data: imageData as! CFData)
                     let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)
                     
-                    let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
+                    self.image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
                     
                     
                     self.tempImageView.isHidden = false
-                    self.tempImageView.image = image
+                    self.tempImageView.image = self.image
                     self.view.addSubview(self.tempImageView)
                 }
             })
         }
+        print("captured image: \(self.image)")
     }
     
     var didTakePhoto = Bool()
     
     func didPressTakeAnother() {
+        //var image : UIImage?
         if didTakePhoto == true {
             tempImageView.isHidden = true
             didTakePhoto = false
+            //return nil
         } else {
             captureSession?.startRunning()
-            didPressTakePhoto()
             didTakePhoto = true
+            didPressTakePhoto()
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         didPressTakeAnother()
+        if let unwrappedImage = self.image {
+            let binaryImage = encodeImageBase64(image: unwrappedImage)
+            createRequest(with: binaryImage)
+        }
+        //didPressTakeAnother()
+    }
+//*****************************************************************************************************//
+    
+    //Networking
+    func encodeImageBase64(image : UIImage) -> String {
+        var imageData = UIImagePNGRepresentation(image)
+        
+        //resize image if it exceeds API size limit
+        if ((imageData?.count)! > 2097152) {
+            let oldSize : CGSize = image.size
+            let newSize: CGSize = CGSize(width: 800, height: oldSize.height / oldSize.width * 800)
+            imageData = resizeImage(imageSize: newSize, image: image)
+        }
+        
+        return imageData!.base64EncodedString(options: .endLineWithCarriageReturn)
+    }
+    
+    func createRequest(with imageBase64 : String) {
+        print("reached here")
+        //create request URL
+        var request = URLRequest(url: googleURL)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        //request.addValue(Bundle.main.bundleIdentifier ?? "", forHTTPHeaderField: "X-Ios-Bundle-Identifier")
+        
+        //Build API request
+        let jsonRequest = [
+            "requests": [
+                "image": [
+                    "content": imageBase64
+                ],
+                "features": [
+                    [
+                        "type": "LOGO_DETECTION",
+                        "maxResults": 10
+                    ]
+                ]
+            ]
+        ]
+        
+        let jsonObject = JSON(jsonDictionary : jsonRequest)
+        
+        //Serialize the JSON
+        guard let data = try? jsonObject.rawData() else {
+            return
+        }
+        
+        request.httpBody = data
+        
+        // Run the request on a background thread
+        DispatchQueue.global().async { self.runRequestOnBackgroundThread(_request: request) }
+    }
+    
+    func runRequestOnBackgroundThread(_request : URLRequest) {
+        let task: URLSessionDataTask = session.dataTask(with: _request) { (data, response, error) in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "")
+                return
+            }
+            
+            self.analyzeResults(dataToParse : data)
+        }
+        
+        task.resume()
+    }
+//*****************************************************************************************************//
+    
+    //Image Processing
+    func resizeImage(imageSize : CGSize, image : UIImage) -> Data {
+        UIGraphicsBeginImageContext(imageSize)
+        image.draw(in: CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        let resizedImage = UIImagePNGRepresentation(newImage!)
+        UIGraphicsEndImageContext()
+        return resizedImage!
+    }
+    
+    func analyzeResults(dataToParse : Data) {
+        //Update UI on main thread
+        DispatchQueue.main.async(execute: {
+            //parse results
+            let json = JSON(data : dataToParse)
+            let errorObj : JSON = json["error"]
+            
+            //TODO: display
+            
+            if (errorObj.dictionaryValue != [:]) {
+                print("Error code \(errorObj["code"]): \(errorObj["message"])")
+            } else {
+                print(json)
+                let responses : JSON = json["responses"][0]
+                
+                //Get logo annotations
+                let logoAnnotations : JSON = responses["logoAnnotations"]
+                let numLogos : Int = logoAnnotations.count
+                
+                if numLogos > 0 {
+                    print(logoAnnotations[0]["description"].stringValue)
+                } else {
+                    print("No logos detected")
+                }
+            }
+        })
     }
 }
